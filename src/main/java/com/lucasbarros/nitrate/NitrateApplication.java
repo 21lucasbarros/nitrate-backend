1c5e112f76f0588af648ccb6bbd082a27882004d
Lucas Barros Simon <lucasweacked21@gmail.com>
2026-07-28 05:16:28 -0300

    feat: tomei vergonha na cara e vou comecar o projeto do back-end do Nitrate.

---
+++ src/main/java/com/lucasbarros/nitrate/NitrateApplication.java
@@ -0,0 +1,13 @@
+package com.lucasbarros.nitrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasbarros/nitrate/NitrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasbarros.nitrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
