4fa6ff88f0d810e55b20692adc495a40d5d71c2a
Lucas Barros Simon <lucasweacked21@gmail.com>
2026-08-11 18:58:07 -0300

    feat: criei a entidade de User para fazer o cadastro e login de usuarios.

---
+++ src/main/java/com/lucasbarros/nitrate/entities/User.java
@@ -0,0 +1,87 @@
+package com.lucasbarros.nitrate.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    private static final long serialVersion = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    private Instant createdAt = Instant.now();
+
+    public User() {
+    }
+
+    public User(String name, String email, String passwordHash) {
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        return Objects.equals(id, ((User) o).id);
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(id); }
+}
